a50fceb9e299a9977b2284d92f68a22746f2685e
Ajmal Hussain HY <ajmalhussainhy@gmail.com>
2018-08-07 20:22:43 +0530

    Revert "Added Important Alert and meta informations"
    This reverts commit 427bf177d9cc37c1a55b9f6e6d17a7f2d2b25ac8.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-/* Hussain Lodge, Tirunelveli. */
-/* Main JavaScript file */
-/* Created by Ajmal Hussain Hy, https://ajmalhussain.in */
-
 $('.navbar-toggler').on('click', function(e) {
     $('.navbar').toggleClass("bg-dark"); //you can list several class names 
     e.preventDefault();
   });
-
-$(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-// navbar menu smooth scroller
-$('a[href^="#"]').on('click', function(event) {
-  var target = $(this.getAttribute('href'));
-  if( target.length ) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-          scrollTop: target.offset().top
-      }, 1000);
-  }
-});
